0fc3f8dc8f9755cded0e011db36db05ba6dd07b2
jugosack <jugoslav_ackoski@yahoo.com>
2023-04-26 23:01:53 +0200

    Add changes in item.rb for adding additional methods

--- item.rb
+++ item.rb
@@ -18,5 +18,14 @@ class Item
     @archived = true if can_be_archived?
   end
 
+  def add_genre(genre)
+    @genre = genre
+    genre.items << self unless genre.items.include?(self)
+  end
+
+  def add_author(author)
+    @author = author
+  end
+
   private :can_be_archived?
 end
