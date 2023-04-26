5a06291de4744a31ae77ce6f07ba4b7ee52d66e5
jugosack <jugoslav_ackoski@yahoo.com>
2023-04-26 23:04:07 +0200

    Add music_album.rb

--- classes/music_albums.rb
+++ classes/music_albums.rb
@@ -1,2 +1,16 @@
-class MusicAlbums
+require_relative '../item'
+
+class MusicAlbum < Item
+  attr_accessor :on_spotify
+
+  def initialize(on_spotify, publish_date = 'Uknown')
+    super(publish_date)
+    @on_spotify = on_spotify
+  end
+
+  private
+
+  def can_be_archived?
+    super && @on_spotify
+  end
 end
