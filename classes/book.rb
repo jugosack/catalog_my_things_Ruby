17c6a85e66ce60c4f930a88b494815573ba2bf21
mrprotocoll <lekanvgbg@gmail.com>
2023-04-28 06:37:17 +0100

    Fixed Linters error

--- classes/book.rb
+++ classes/book.rb
@@ -1,16 +1,17 @@
 require_relative '../item'
 
 class Book < Item
-    attr_accessor :publisher, :cover_state
-    def initialize(publisher, publish_date, cover_state = "good", archived: false)
-        super(publish_date, archived: archived)
-        @publisher = publisher
-        @cover_state = cover_state
-    end
+  attr_accessor :publisher, :cover_state
 
-    private
+  def initialize(publisher, publish_date, cover_state = 'good', archived: false)
+    super(publish_date, archived: archived)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
 
-    def can_be_archived?
-        super && @cover_state == 'bad'
-    end
+  private
+
+  def can_be_archived?
+    super && @cover_state == 'bad'
+  end
 end
