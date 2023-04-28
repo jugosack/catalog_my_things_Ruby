2ab0b54a8c44441d8e8c71b7e22dc77b02c17049
mrprotocoll <lekanvgbg@gmail.com>
2023-04-28 05:51:49 +0100

    Added test for label class

--- classes/label.rb
+++ classes/label.rb
@@ -1,7 +1,7 @@
 class Label 
 
-    attr_accessor :title, :color
-    attr_reader :id, :items
+    attr_accessor :title, :color, :items
+    attr_reader :id
 
     def initialize(title, color)
         @id = Random.rand(1..1000)
---
+++ spec/label_spec.rb
@@ -0,0 +1,23 @@
+require_relative '../classes/label'
+require_relative '../item'
+
+describe Genre do
+  before :each do
+    @label = Label.new('new', 'green')
+  end
+
+  context '#new' do
+    it 'accepts (2) parameters and returns an instance of Label' do
+      expect(@label).to be_an_instance_of(Label)
+    end
+  end
+
+  context '#add_item' do
+    it 'takes an instance of item and add it to the collection of items' do
+      @item = Item.new('2010-1-15')
+      @label.add_item(@item)
+      expect(@label.items.length).to eq(1)
+      expect(@item.label).to be_an_instance_of(Label)
+    end
+  end
+end
