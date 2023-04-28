223049b7589e4cd4235f38bffdc0463c66c3c5ef
mrprotocoll <lekanvgbg@gmail.com>
2023-04-28 05:15:44 +0100

    Updated book and label classes

--- classes/book.rb
+++ classes/book.rb
@@ -1,3 +1,13 @@
-class Book
-    
+require '../item'
+
+class Book < Item
+    def initialize(publisher, cover_state = "")
+        @publisher = publisher
+        @cover_state = cover_state
+    end
+
+    private
+
+    def can_be_archived?
+    end
 end
--- classes/label.rb
+++ classes/label.rb
@@ -1,5 +1,15 @@
-class Label
-    def initialize
-        
+class Label 
+
+    attr_accessor :title, :color
+    attr_reader :id, :items
+
+    def initialize(title, color)
+        @id = Random.rand(1..1000)
+        @title = title
+        @color = color
+        @items = []
+    end
+
+    def add_item(item)
     end
 end
